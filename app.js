c763611a829bcb97ace75b235a607b306ebc98b8
Soyeb <tssoyeb@gmail.com>
2022-03-02 19:37:47 +0600

    Cleaning

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+// display block,none property
 const toggleShow=(id,displyStyle)=>{
   document.getElementById(id).style.display=displyStyle;
   
 }
 
+// getting input key and create dynamic api 
 const searchPhone=()=>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -15,6 +17,7 @@ const searchPhone=()=>{
     .then(res=>res.json())
     .then(phone=>{
       // console.log(phone)
+// Error handling---------------->
       if(phone.data==0){
         toggleShow('error','block');
         toggleShow('spinner','none');
@@ -33,11 +36,12 @@ const searchPhone=()=>{
     })
 }
 
+// Display all result after searching by a name 
 const displayAllPhone =info=>{
 // console.log(info);
 const displaySearchResult = document.getElementById('search-result');
 displaySearchResult.textContent='';
-// console.log(info[0]);
+console.log(info[0]);
 for (let i=0;i<20;i++){
     let data = info[i];
     // toggleShow('search-result','block')
@@ -46,7 +50,7 @@ for (let i=0;i<20;i++){
     div.classList.add('col','shadow', 'p-3')
     div.innerHTML=`
     <div class="card h-100 bg-secondary shadow">
-    <img src="${data.image}" class="card-img-top w-75  h-75 mx-auto rounded" alt="...">
+    <img src="${data.image?data.image:'Not available'}" class="card-img-top w-75  h-75 mx-auto rounded" alt="...">
     <div class="card-body">
       <h5 class="card-title text-light">${data.phone_name}</h5>
       <p class="card-text text-light">${data.brand}</p>
@@ -56,19 +60,24 @@ for (let i=0;i<20;i++){
     `
     displaySearchResult.appendChild(div);
     toggleShow('spinner','none');
-
+}
 }
 
-
-}
+//Getting API of single phone details------------>
 const phoneDetailsButton=id=>{
   toggleShow('spinner','block');
   // console.log(id);
   const url=`https://openapi.programming-hero.com/api/phone/${id}`
   fetch(url)
   .then(res=>res.json())
-  .then(info=>phoneDetails(info.data))
+  .then(info=>{
+    
+    console.log(info);
+    phoneDetails(info.data)
+  })
 }
+
+//Getting everything from api about phone details
 const phoneDetails =(datas)=>{
   
   // console.log(datas)
@@ -91,7 +100,7 @@ const phoneDetails =(datas)=>{
   </div>
   <div class="card-body overflow-auto w-50">
     <h5 class="card-title">${datas.name}</h5>
-    <p class="card-text ">Brand : ${datas.brand} <br>Release Date: ${datas.releaseDate?datas.releaseDate:'Not Available'} <br><b class="text-info">Main Features:</b><br>ChipSet : ${dataMainFeature.chipSet} <br> Display Size : ${dataMainFeature.displaySize}<br>Memory : ${dataMainFeature.memory} <br>Storage : ${dataMainFeature.storage} <br><b class="text-info">Sensors :</b> <br> ${sensor}<br> <b class="text-info">Others :</b><br>Bluetooth : ${dataOthers.bluetooth}<br>GPS : ${dataOthers.GPS}<br>NFC : ${dataOthers.NFC}, Radio : ${dataOthers.Radio}<br>USB : ${dataOthers.USB}<br>WLAN : ${dataOthers.WLAN} </p>
+    <p class="card-text ">Brand : ${datas.brand} <br>Release Date: ${datas.releaseDate?datas.releaseDate:'Not Available'} <br><b class="text-info">Main Features:</b><br>ChipSet : ${dataMainFeature.chipSet} <br> Display Size : ${dataMainFeature.displaySize}<br>Memory : ${dataMainFeature.memory} <br>Storage : ${dataMainFeature.storage} <br><b class="text-info">Sensors :</b> <br> ${sensor}<br> <b class="text-info">Others :</b><br>Bluetooth : ${dataOthers.bluetooth?dataOthers.bluetooth:'Not Available'}<br>GPS : ${dataOthers.GPS}<br>NFC : ${dataOthers.NFC}, Radio : ${dataOthers.Radio}<br>USB : ${dataOthers.USB}<br>WLAN : ${dataOthers.WLAN} </p>
   </div>
 
   `
